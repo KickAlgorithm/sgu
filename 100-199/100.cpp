262c3ab72e69cfacb3cd2c1160e8eb04d8d43c36
Zhenchao Gan <303542123@qq.com>
2015-06-12 16:12:44 +0800

    100. A+B

---
+++ 100-199/100.cpp
@@ -0,0 +1,30 @@
+/*
+Read integers A and B from input file and write their sum in output file.
+
+Input
+
+Input file contains A and B (0<A,B<10001).
+
+Output
+
+Write answer in output file.
+
+Sample Input
+
+ 5 3
+Sample Output
+
+ 8
+*/
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    cout << a+b;
+
+    return 0;
+}
